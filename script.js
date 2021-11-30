ef20f9b3554aef18e2ffeae69f8938c24f76dce6
Tyler Everett <tylertechnotrade@gmail.com>
2021-11-29 16:36:14 -0800

    changes

--- script.js
+++ script.js
@@ -2,12 +2,21 @@
 var generateBtn = document.querySelector("#generate");
 
 var lowerCase = [
-   "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 var upperCase = [
   "A", "B", "C", "D", "D", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-   
+
+var numerical = [
+  "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+var special = [
+  "!", "@", "#", "$", "%", "^", "&", "*",]
+
   
+
+
+
 
 
 // Write password to the #password input
